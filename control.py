c778f227af28123185aaec86713c1e96fae13558
SANDEEP B S <sandeepbs2000@gmail.com>
2023-04-24 16:30:05 +0530

    if elif else

---
+++ control.py
@@ -0,0 +1,21 @@
+# controlls in python
+print("controlls")
+
+# if , else
+
+number=int(input("enter a number")) #need to convert to intiger value,default input is string
+if number <= 0 : #if entered number is less than zero or equals zero then print
+    print("enter a positive number")
+else :
+    print("go to next step")
+
+#elif
+
+num=int(input("enter a number"))
+if num < 0:
+    print("negative number")
+elif num == 0:  # [== equal sign]
+    print("entered number is zero")
+else :
+    print("positive number")
+
